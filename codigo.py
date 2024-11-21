06847b683a9376fd8fb3072de4be9c1fbee55051
criiss-s <elweoncris@gmail.com>
2024-11-21 10:29:17 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,9 +1,12 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
 import altair as alt
 import base64
 
-# Fondo
+
+
+#fondo
 def get_base64_of_bin_file(bin_file):
     with open(bin_file, 'rb') as f:
         data = f.read()
@@ -26,7 +29,10 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 st.sidebar.image("letras.png")
 opcion = st.sidebar.selectbox('Selecciona una sección', ['Información', 'Campeones', 'Competitivo', 'Acerca de'])
 
+
 st.image("League-of-Legends-logo.png")
+
+
 
 if opcion == 'Información':
     st.markdown("<h1 style='color: white;'>Información</h1>", unsafe_allow_html=True)
@@ -39,15 +45,38 @@ elif opcion == 'Competitivo':
     st.markdown("<h1 style='color: white;'>Competitivo</h1>", unsafe_allow_html=True)
     video_url = "https://www.youtube.com/watch?v=Kv2rswmxBVs"
     st.video(video_url)
+    st.markdown(
+    """
+    <div style="margin-left: -75px; margin-right: -75px;">
+        <h3 style="color: white;">¿Que es el competitivo de league of legends?</h3>
+    </div>
+    """,
+    unsafe_allow_html=True
+    )
     st.markdown("""
     <div style="margin-left: -75px;">
-        <h3 style="color: white;">¿Qué es el competitivo de League of Legends?</h3>
-        <p style='color: white;'>El competitivo de League of Legends (LoL) se refiere a los torneos y ligas organizados por Riot Games donde equipos profesionales compiten por premios y reconocimiento. Estos torneos incluyen eventos regionales, nacionales e internacionales, como la League of Legends Championship Series (LCS), la League of Legends European Championship (LEC), y el Campeonato Mundial de League of Legends (Worlds), todo esto se realiza en amplios lugares para que así los fanáticos de este juego puedan asistir de forma presencial y vivir una experiencia inolvidable.</p>
+    <div style="margin-right: -75px;">
+    <div style="text-align: justify; text-justify: inter-word;">
+        <p style='color: white;'>El competitivo de League of Legends (LoL) se refiere a los torneos y ligas organizados por Riot Games donde equipos profesionales compiten por premios y reconocimiento. Estos torneos incluyen eventos regionales, nacionales e internacionales, como la League of Legends Championship Series (LCS), la League of Legends European Championship (LEC), y el Campeonato Mundial de League of Legends (Worlds), todo esto se realiza en amplios lugares para que asi los fanaticos de este juego puedan asistir de forma presencial y vivir una experiencia inolvidable. p>
     </div>
     """, unsafe_allow_html=True)
     st.image("17285869617624.jpg")
-    st.markdown("""
+    st.markdown(
+    """
     <div style="margin-left: -75px; margin-right: -75px;">
         <h3 style="color: white;">Brackets de Worlds</h3>
     </div>
-    """, unsafe_allow
+    """,
+    unsafe_allow_html=True
+    )
+    st.markdown("""
+    <div style="margin-left: -75px;">
+    <div style="margin-right: -75px;">
+    <div style="text-align: justify; text-justify: inter-word;">
+        <p style='color: white;'>Dentro del competitivo del league of legends se implementa una clase de bracket (Cuadro) el cual permite mostrar el progreso de los equipos a lo largo de un torneo, asi para que la gente sepa si su equipo favorito paso a la siguiente fase. El bracket mostrado corresponde al bracket suizo de los worlds 2024 con equipos reconocidos como G2, GenG, T1, FNC, etc. p>
+    </div>
+    """, unsafe_allow_html=True)
+
+
+elif opcion == 'Acerca de':
+    st.write('Aquí se mostraría la información adicional.')
