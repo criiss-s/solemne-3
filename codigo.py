525866dc687cebaf83f31b5a2677e14979a4e28a
criiss-s <elweoncris@gmail.com>
2024-11-21 10:56:58 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -60,6 +60,20 @@ elif opcion == 'Competitivo':
         <p style='color: white;'>El competitivo de League of Legends (LoL) se refiere a los torneos y ligas organizados por Riot Games donde equipos profesionales compiten por premios y reconocimiento. Estos torneos incluyen eventos regionales, nacionales e internacionales, como la League of Legends Championship Series (LCS), la League of Legends European Championship (LEC), y el Campeonato Mundial de League of Legends (Worlds), todo esto se realiza en amplios lugares para que asi los fanaticos de este juego puedan asistir de forma presencial y vivir una experiencia inolvidable. p>
     </div>
     """, unsafe_allow_html=True)
+     st.markdown(
+    """
+    <div style="margin-left: -75px; margin-right: -75px;">
+        <h3 style="color: white;">Rangos Competitivos</h3>
+    </div>
+    """,
+    unsafe_allow_html=True
+         st.markdown("""
+    <div style="margin-left: -75px;">
+    <div style="margin-right: -75px;">
+    <div style="text-align: justify; text-justify: inter-word;">
+        <p style='color: white;'>asdsadasda p>
+    </div>
+    """, unsafe_allow_html=True)
     st.image("17285869617624.jpg")
     st.markdown(
     """
